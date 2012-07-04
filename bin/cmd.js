f29f0c73d63f10f1399d89f238d41a49f9b00499
Dominic Tarr <dominic.tarr@gmail.com>
2012-07-05 02:41:08 +1200

    change all path.exists* -> fs.exists*

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,8 +3,6 @@ var path = require('path');
 var fs = require('fs');
 var exec = require('child_process').exec;
 
-var existsSync = fs.exists || path.existsSync
-
 var request = require('request');
 var semver = require('semver');
 
@@ -39,8 +37,8 @@ withConfig(function (config) {
         }
         else {
             var dir = gitDir(process.cwd());
-            if (!existsSync(dir + '/.travis.yml')
-            && existsSync(dir + '/package.json')) {
+            if (!fs.existsSync(dir + '/.travis.yml')
+            && fs.existsSync(dir + '/package.json')) {
                 var pkg = JSON.parse(fs.readFileSync(dir + '/package.json'));
                 
                 var sv = (pkg.engines || {}).node || '>=0.4';
